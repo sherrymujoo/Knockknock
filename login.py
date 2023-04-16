8a8d65397734ef1aa73edbe49e6d77a59942c088
Sharan <130827526+sherrymujoo@users.noreply.github.com>
2023-04-16 12:26:23 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,9 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input ( "Password >")
+if username == "mark" and password == "mark":
+  print("Welcome Mark!")
+elif username == "roxanne" and password == "roxanne":
+  print ("Welcome Roxanne!")
+else:
+  print("Go away!")
